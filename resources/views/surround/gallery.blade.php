0a65e08782988dc4191da9055b802006729e5ee3
Sylver <vitalissylver@gmail.com>
2024-10-04 15:28:37 +0100

    gallery fix

--- resources/views/surround/gallery.blade.php
+++ resources/views/surround/gallery.blade.php
@@ -4,17 +4,15 @@
 
 @section('content')
 
-    <div class="flex flex-col h-screen w-full bg-[url('/images/decor.jpg')] bg-cover bg-center bg-no-repeat">
+    <div class="flex flex-col h-full w-full bg-[url('/images/decor.jpg')] bg-cover bg-center bg-no-repeat">
         {{-- <img src="/images/decor.jpg" alt="decor" class="w-full"> --}}
 
         <div class="flex flex-col items-center w-full my-4">
             <p class="lg:text-4xl text-3xl font-lato font-extrabold text-white my-3 lg:my-7">Gallery</p>
         </div>
-
         <div class="flex flex-col items-center w-full h-full">
-            <div class="flex flex-col bg-[#1b998b] h-full w-full mx-20 py-10 lg:px-10 lg:py-10 md:px-10 md:py-10">
-                <div
-                    class="w-full py-20 px-8 lg:px-20 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-16">
+            <div class="flex flex-col w-full bg-[#1b998b] py-10 lg:px-10 lg:py-10 md:px-10 md:py-10">
+                <div class="w-full py-20 px-8 lg:px-20 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-16">
                     @foreach ($galleries as $gallery)
                         @if ($gallery->type == 1)
                             <img src="{{ Storage::url($gallery->url) }}" alt="{{ $gallery->title }}" class="h-[351px] w-full">
